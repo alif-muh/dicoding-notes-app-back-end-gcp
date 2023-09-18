348e197a30bd8193080ca3e8ca4dfc493e1da96c
Muhammad Alif <contact.alif02@gmail.com>
2023-09-18 16:11:07 +0700

    fixed typo

--- src/handler.js
+++ src/handler.js
@@ -97,7 +97,7 @@ const editNoteByIdHandler = (request, h) => {
 
     const response = h.response({
       status: 'success',
-      message: 'Catatan berhasil diperharui',
+      message: 'Catatan berhasil diperbarui',
     });
     response.code(200);
     return response;
